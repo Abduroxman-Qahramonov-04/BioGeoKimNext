fe7f1d44faf0c8225912f42768edf0926f86dc0c
Abdurahmon-Dev <abdurahmonmirobidov2004@gmail.com>
2021-04-28 23:30:00 +0500

    add mobile version

---
+++ main.js
@@ -0,0 +1,28 @@
+function OpenMenu(){
+    var iconMenuOpen = document.getElementById('IconMenuOpen')
+    iconMenuOpen.style.display = 'none'
+
+    var iconMenuClose = document.getElementById('IconMenuClose')
+    iconMenuClose.style.display = 'block'
+    iconMenuClose.addEventListener('click',CloseMenu)
+
+    var SubjectsMobileVersion = document.getElementById('SubjectsMobileVersion')
+    SubjectsMobileVersion.style.display = 'block'
+}
+
+function CloseMenu(){
+    var iconMenuOpen = document.getElementById('IconMenuOpen')
+    iconMenuOpen.style.display = 'block'
+
+    var iconMenuClose = document.getElementById('IconMenuClose')
+    iconMenuClose.style.display = 'none'
+    
+
+    var SubjectsMobileVersion = document.getElementById('SubjectsMobileVersion')
+    SubjectsMobileVersion.style.display = 'none'
+}
+
+window.addEventListener('load',function(){
+    var iconMenuOpen = document.getElementById('IconMenuOpen')
+    iconMenuOpen.addEventListener('click',OpenMenu)
+})
