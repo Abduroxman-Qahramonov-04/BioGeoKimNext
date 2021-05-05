050dadfed09fd99fdd0fed96f6d60b53830644a1
Abdurahmon-Dev <abdurahmonmirobidov2004@gmail.com>
2021-05-05 16:55:55 +0500

    fix bags

--- main.js
+++ main.js
@@ -4,5 +4,9 @@ window.onload = function() {
         document.querySelector('.categories').classList.toggle('active')
         document.querySelector('.line').classList.toggle('active')
         document.querySelector('.language-contanier').classList.toggle('active')
+        document.querySelector('#LanguageUZ').innerHTML = 'O`zbekcha'
+        document.querySelector('#LanguageRu').innerHTML = 'Русский'
+        document.querySelector('#LanguageEn').innerHTML = 'English'
+
     })
 }
