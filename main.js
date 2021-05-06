82b551385acfbc22eb6f7dcd42bce157d30173ec
Abdurahmon-Dev <abdurahmonmirobidov2004@gmail.com>
2021-05-06 17:33:14 +0500

    new html

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 window.onload = function() {
-        document.querySelector('.icon-menu-contanier').addEventListener('click', function() {
-        document.querySelector('.icon-menu-contanier').classList.toggle('active')
-        document.querySelector('.categories').classList.toggle('active')
-        document.querySelector('.line').classList.toggle('active')
-        document.querySelector('.language-contanier').classList.toggle('active')
+        document.querySelector('.header-burger').addEventListener('click', function() {
+        document.querySelector('.header-burger').classList.toggle('active')
+        document.querySelector('.menu-box').classList.toggle('active')
         document.querySelector('#LanguageUZ').innerHTML = 'O`zbekcha'
-        document.querySelector('#LanguageRu').innerHTML = 'Русский'
-        document.querySelector('#LanguageEn').innerHTML = 'English'
+        document.querySelector('#LanguageRU').innerHTML = 'Русский'
+        document.querySelector('#LanguageEN').innerHTML = 'English'
 
     })
 }
