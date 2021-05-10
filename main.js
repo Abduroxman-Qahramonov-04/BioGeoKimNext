6dd778ba1b50bf019ff7754b791a577f00013d54
Abdurahmon-dev <abdurahmonmirobidov2004@gmail.com>
2021-05-10 22:42:14 +0500

    add footer part

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+function ToggleClass(){
+    document.querySelector('.header-burger').classList.toggle('active')
+    document.querySelector('.menu-box').classList.toggle('active')
+    document.querySelector('#LanguageUZ').innerHTML = 'O`zbekcha'
+    document.querySelector('#LanguageRU').innerHTML = 'Русский'
+    document.querySelector('#LanguageEN').innerHTML = 'English'
+}
+
 window.addEventListener('load', function() {
-        document.querySelector('.header-burger').addEventListener('click', function() {
-        document.querySelector('.header-burger').classList.toggle('active')
-        document.querySelector('.menu-box').classList.toggle('active')
-        document.querySelector('#LanguageUZ').innerHTML = 'O`zbekcha'
-        document.querySelector('#LanguageRU').innerHTML = 'Русский'
-        document.querySelector('#LanguageEN').innerHTML = 'English'
-        
-    })
+        document.querySelector('.header-burger').addEventListener('click', ToggleClass)
 })
